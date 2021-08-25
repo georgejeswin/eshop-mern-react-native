a2b1c9865882e8ca07f36803a202ecd4a4fc9644
Jeswin George <jeswingeorge@Jeswins-MacBook-Air.local>
2021-08-25 13:57:38 +0530

    order update added

--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -6,9 +6,28 @@ const router = express.Router();
 
 router.get("/", async (req, res) => {
   try {
-    const orderList = await Order.find();
+    const orderList = await Order.find()
+      .populate("user", "name")
+      .sort({ dateOrdered: -1 });
     if (!orderList) return res.status(404).json({ success: false });
     res.status(200).send(orderList);
+  } catch (error) {
+    res.status(404).json({
+      error: error.message,
+    });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const order = await Order.findById(req.params.id)
+      .populate("user", "name")
+      .populate({
+        path: "orderItems",
+        populate: { path: "product", populate: "category" },
+      });
+    if (!order) return res.status(404).json({ success: false });
+    res.status(200).send(order);
   } catch (error) {
     res.status(404).json({
       error: error.message,
@@ -51,6 +70,22 @@ router.post("/", async (req, res) => {
     res.status(500).json({
       error: error.message,
     });
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  try {
+    const order = await Order.findByIdAndUpdate(
+      req.params.id,
+      {
+        status: req.body.status,
+      },
+      { new: true }
+    );
+    if (!order) res.status(400).send("Order cannot be updated");
+    return res.status(200).send(order);
+  } catch (error) {
+    res.status(400).json({ error: error });
   }
 });
 
