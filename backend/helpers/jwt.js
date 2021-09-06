2180cddac926923f84d9fce0df7a7bc8438ff484
Jeswin George <jeswingeorge@Jeswins-MacBook-Air.local>
2021-09-06 23:47:12 +0530

    images backend get req fix

--- backend/helpers/jwt.js
+++ backend/helpers/jwt.js
@@ -15,6 +15,7 @@ function authJwt() {
       { url: /\/api\/v1\/products(.*)/, methods: ["GET", "OPTIONS"] },
       { url: /\/api\/v1\/categories(.*)/, methods: ["GET", "OPTIONS"] },
       { url: /\/api\/v1\/orders(.*)/, methods: ["GET", "OPTIONS", "POST"] },
+      { url: /\/public\/uploads(.*)/, methods: ["GET", "OPTIONS", "POST"] },
       `${api}/users/login`,
       `${api}/users/register`,
     ],
